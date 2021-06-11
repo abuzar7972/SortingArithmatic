47e40616a7a075f1ec4237b2ab8f5c7d4448045a
abuzar7972 <abushaikh7972@gmail.com>
2021-06-12 03:25:22 +0530

    compute changes : a*b+c

--- SortingArithmatic.sh
+++ SortingArithmatic.sh
@@ -2,5 +2,5 @@
 read -p "Enter value for Arithmatic calculation a:: " a
 read -p "Enter value for Arithmatic calculation b:: " b
 read -p "Enter value for Arithmatic calculation c:: " c
-result=$(($a+$b*$c))
+result=$(($a*$b+$c))
 echo "$result"
