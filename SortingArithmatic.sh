33a580c5a31e8f00dc673a328689c3528c314c1f
abuzar7972 <abushaikh7972@gmail.com>
2021-06-12 03:44:38 +0530

    compute changes : a%b+c

--- SortingArithmatic.sh
+++ SortingArithmatic.sh
@@ -1,6 +1,14 @@
 #!/bin/bash -x
+declare -A dict 
 read -p "Enter value for Arithmatic calculation a:: " a
 read -p "Enter value for Arithmatic calculation b:: " b
 read -p "Enter value for Arithmatic calculation c:: " c
-result=$(($a%$b+$c))
-echo "$result"
+a=$(($a+$b*$c))
+dict[0]="$a"
+b=$(($a*$b+$c))
+dict[1]="$b"
+c=$(($c+$a/$b))
+dict[2]="$c"
+d=$(($a%$b+$c))
+dict[3]="$d"
+
